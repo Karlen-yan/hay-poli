6f9b4a1b179d4246207621e2fe976648266c3d12
Kar_Yan <102352076+Karlenjak@users.noreply.github.com>
2022-11-16 23:05:41 +0100

    Cambios de html menu con vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,72 @@
 <template>
-<div id="app">
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg='hola'/>
-  <NavBar /> 
+  <div id="app">
+    <header>
+      <nav>
+          <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Servicio(Prs_Rop)</a></li>
+            <li><a href="#">Contactos</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Buscador</a></li>
+            <li><a href="#">Registrarse(bolsa)</a></li>
+          </ul>
+      </nav>
+      <!-- <Navbar >
+            Header
+           </Navbar> -->
+    </header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import NavBar from './components/NavBar.vue';
+// import NavBar from './components/NavBar.vue';
+
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    // eslint-disable-next-line vue/no-unused-components
-    HelloWorld,
-    NavBar
-  }
-}
+    //  NavBar
+  },
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+
+body{
+  margin: 0;
+}
+nav {
+  background-color: rgba(0, 0, 0, 0.707);
+  opacity: .65;
+  padding: 12px 0 12px 0 ;
+
+}
+
+nav ul {
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  margin: 0;
+}
+nav ul a {
+  color: white;
+  text-decoration: none;
+  padding: 0 20px 0 20px;
+}
+
+span.icon{
+  width: 20px;
+  height: 20px;
+  background-size: 100% 100%;
+}
+.hay-poli{
+  background-image: url();
 }
 </style>
