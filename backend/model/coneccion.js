e0ee015780e7db21bb8d1cf3547e72a74db39cb5
Kar_Yan <102352076+Karlenjak@users.noreply.github.com>
2023-02-23 19:15:29 +0100

    Anadir

--- backend/model/coneccion.js
+++ backend/model/coneccion.js
@@ -1,2 +1,8 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
+
+const URL = process.env.MONGODB_URI;
+mongoose.connect(URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
