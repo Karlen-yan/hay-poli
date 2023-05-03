8e7b4ca6367852ccb1b19a67a4719bef82f9a76b
Kar_Yan <102352076+Karlenjak@users.noreply.github.com>
2023-05-03 22:41:07 +0200

    allowedHosts cambios a all

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,11 +5,13 @@ module.exports = {
       'Access-Control-Allow-Origin': '*'
     },
     allowedHosts: [
+      'all',
       'localhost',
       '192.168.1.1',
       '3.75.158.163',
       '3.125.183.140',
-      '35.157.117.28'
+      '35.157.117.28',
+      'https://hay-poli-frontend.onrender.com/'
     ],
   },
   
